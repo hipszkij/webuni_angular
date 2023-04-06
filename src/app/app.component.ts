56d3fbb2762140acd2263dc8d50b2d9609801348
Hipszki János <hipszki.janos@mxnet.hu>
2023-04-06 10:51:19 +0200

    lesson 3 órai munka

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Első angular alkalmazásom';
+  title: string = 'Első angular alkalmazásom';
   counter = {name: 'Mosogatás', id: 1};
-  aim = 5;
-  logoSrc = 'https://angular.io/assets/images/logos/angular/angular.png';
+  aim: number  = 5;
+  logoSrc: string = 'https://angular.io/assets/images/logos/angular/angular.png';
+  isComplete: boolean = false;
+  counterCount: number = 0;
+  counters: {name: string, id: number, aim: number}[] = [];
 
   countChanged(count: number) {
     if (count == this.aim) {
+      this.isComplete = true;
       alert('Cél elérve');
+    }
+
+    if (count == 3) {
+      this.aim = 10;
+      this.counter = {name: 'Teregetés', id: 2};
+    }
+  }
+
+  counterCountChanged() {
+    const diff = this.counterCount - this.counters.length;
+
+    if (diff > 0) {
+      const newCounters = new Array(diff).fill(undefined).map((_, i) => {
+        return {name:'', id: i + this.counterCount, aim: 10};
+      });
+
+      this.counters = [...this.counters, ...newCounters];
+    } else if (diff < 0) {
+      this.counters.splice(this.counterCount);
     }
   }
 }
--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,24 +1,42 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
   styleUrls: ['./counter.component.css']
 })
-export class CounterComponent implements OnInit {
+export class CounterComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy {
   @Input() aim = 10;
   @Input() counter: {name: string, id: number} | undefined;
   @Output() countChange = new EventEmitter<number>();
   count = 0;
+  @ViewChild('btn') buttonRef: ElementRef<HTMLButtonElement> | undefined;
 
-  constructor() { }
-
+  constructor() {
+    console.log('constructor');
+  }
   ngOnInit(): void {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges', changes);
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+    //this.buttonRef!.nativeElement.textContent = 'Set from TS';
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
   }
 
   incrementCounter() {
     this.count++;
     this.countChange.emit(this.count);
   }
+
+  get isComplete(): boolean {
+    return this.count >= this.aim;
+  }
+
 
 }
