af4ab84b7147d42ffdb4334d9cd09435d83823fe
Hipszki János <hipszki.janos@mxnet.hu>
2023-04-23 15:39:39 +0200

    lesson_6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DemoComponent } from './demo/demo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'demo',
+    loadChildren: () => import("./demo/demo.module").then(m => m.DemoModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/demo/demo.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DemoService {
+
+  constructor(private http: HttpClient) { }
+
+  getTitle(): Observable<any> {
+    return this.http.get<any>(`https://jsonplaceholder.typicode.com/posts/1`)
+      .pipe(map(post => post.title));
+  }
+}
