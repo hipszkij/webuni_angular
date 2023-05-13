8452db7abfad7bc9a305d012f8914ce48969ce42
Hipszki János <hipszki.janos@mxnet.hu>
2023-05-13 16:50:02 +0200

    lesson7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { VipComponent } from './vip/vip.component';
+import { VipGuard } from './vip.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'vip',
+    component: VipComponent,
+    canActivate: [VipGuard]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
