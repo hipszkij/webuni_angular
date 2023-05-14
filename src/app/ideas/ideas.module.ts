d8e823daca557d903831243c541de5567bd745d6
Hipszki János <hipszki.janos@mxnet.hu>
2023-05-14 19:00:53 +0200

    init

---
+++ src/app/ideas/ideas.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NewIdeaComponent } from './new-idea/new-idea.component';
+import { ListIdeasComponent } from './list-ideas/list-ideas.component';
+import { IdeaComponent } from './idea/idea.component';
+import { VoteComponent } from './vote/vote.component';
+
+
+
+@NgModule({
+  declarations: [
+    NewIdeaComponent,
+    ListIdeasComponent,
+    IdeaComponent,
+    VoteComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class IdeasModule { }
